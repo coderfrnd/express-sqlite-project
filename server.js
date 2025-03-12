61c7a1220d79ba3f0da7b1d32923ac56e950b8e0
Abhishek <warriorsshadow39@gmail.com>
2025-03-12 18:19:27 +0530

    env file added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { router } from "./app/routes/tutorial.route.js";
+import { config } from "dotenv";
+
+config();
+const port = process.env.PORT || 3000;
+console.log("Loaded Environment Variables:", process.env.PORT);
 
 const app = express();
-const port = 3000;
+
 app.use(express.json());
 app.use("/api/tutorial", router);
 app.listen(port, () => {
-  console.log("port is running on 3000");
+  console.log("port is running on", port);
 });
